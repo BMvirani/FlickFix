1b94281a8b48c6c3da45044079cba7dfdf0c39e0
Bhargav Virani <88129140+BMvirani@users.noreply.github.com>
2024-05-11 18:23:48 +0530

    Update MovieList.js

--- src/app/components/MovieList.js
+++ src/app/components/MovieList.js
@@ -1,29 +1,34 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
+import MovieListSkeleton from "./loaders/MovieListSkeleton";
 
 const MovieList = ({ moviesData, title }) => {
   const [movieList, setMovieList] = useState([]);
   const [rootLoader, setRootLoader] = useState(true);
   useEffect(() => {
     setMovieList(moviesData);
-    setTimeout(()=>{
-
-      setRootLoader(false);
-    },1000)
-  }, [moviesData]);
+    if(movieList){
+      setTimeout(() => {
+        
+        setRootLoader(false);
+      }, 1000);
+    }
+  }, [movieList]);
   return (
     <>
-    {!rootLoader ? 
-    <div>
-      <h2 className="movie-list-title"> {title}</h2>
-      <div className="movie-list">
-        {movieList?.map((item, index) => (
-          <MovieCard key={index} movieInfo={item} />
-        ))}
-      </div>
-    </div>:<></>
-  }
+      {!rootLoader ? (
+        <div>
+          <h2 className="movie-list-title"> {title}</h2>
+          <div className="movie-list">
+            {movieList?.map((item, index) => (
+              <MovieCard key={index} movieInfo={item} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <><MovieListSkeleton /></>
+      )}
     </>
   );
 };
